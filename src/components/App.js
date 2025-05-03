02a6f5f23f6aae2195875eac366fefe93eeeb741
Parikshit <Parikshitmali17@gmail.com>
2025-05-03 16:58:49 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,36 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useRef, useState } from 'react';
 
+export default function App() {
+    const inputTextArea=useRef("")
+const [para,setPara]=useState("")
+
+  return (
+    <div style={{ display: 'flex', height: '700px' }}>
+      <textarea 
+        style={{ 
+          flex: 1, 
+          resize: 'none', 
+          padding: '10px', 
+          fontSize: '16px' 
+        }} 
+        placeholder="Type here..." 
+      ref={inputTextArea}
+      onChange={(e)=>{
+        console.log("Change");
+        
+        setPara(e.target.value)}}
+       />
+      <p 
+        style={{ 
+          flex: 1, 
+          margin: 0, 
+          padding: '10px', 
+          fontSize: '16px', 
+          borderLeft: '1px solid #ccc' 
+        }}
+      >
+        {para}
+      </p>
+    </div>
+  );
+}
